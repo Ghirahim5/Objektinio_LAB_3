cc71327f86d48a8bffd4e521391df43cfe54b617
Matas Siaurodinas <147089390+Ghirahim5@users.noreply.github.com>
2024-03-17 15:56:01 +0200

    Add files via upload

---
+++ Projekto failai /Deque/funkcijos.h
@@ -0,0 +1,16 @@
+#ifndef FUNKCIJOS_H
+#define FUNKCIJOS_H
+
+#include <deque>
+#include "studentas.h"
+
+void GeneruotiPazymius(std::deque<Studentas> & Duomenys);
+void GeneruotiPazymiusVardus();
+double Skaitymas(std::deque<Studentas>& Duomenys);
+void RusiuotiSpausdinti(std::deque<Studentas>& Duomenys, double laikas, const std::string& pavadinimas);
+void RankinisIvedimas(std::deque<Studentas>& Duomenys);
+void GeneruotiFailus();
+void RusiuotiGeraisBlogais(std::deque<Studentas>& Duomenys);
+std::deque<double> Testavimas(std::deque<Studentas>& Duomenys);
+
+#endif
